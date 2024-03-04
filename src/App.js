f44135458e78439942e1221a380b3171ed48b837
Priyank-9181 <jaysukhshekhada11@gmail.com>
2024-03-04 14:03:34 +0530

    hook branch

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./HooksExample/pokedex/page/HomePage";
-import PokemonDetails from "./HooksExample/pokedex/page/PokemonDetails";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    // <div>
-    //   <BrowserRouter>
-    //     <Routes>
-    //       <Route path="/" element={<Layout />}>
-    //         <Route index element={<ShinchanFnadom />} />
-    //         <Route path="character/:name" element={<CharacterDetail />} />
-    //         <Route path="contact" element={<Contact />} />
-    //         <Route path="about" element={<About />} />
-    //       </Route>
-    //     </Routes>
-    //   </BrowserRouter>
-    // </div>
-
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<HomePage />} />
-          <Route path="pokemon/:id" element={<PokemonDetails />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div>okk</div>;
 }
 
 export default App;
