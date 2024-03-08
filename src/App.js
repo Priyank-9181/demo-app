9a6f433e0245172fcdd6351f8ba797e73fdf5581
Priyank-9181 <jaysukhshekhada11@gmail.com>
2024-03-08 11:43:50 +0530

    pokedex

--- src/App.js
+++ src/App.js
@@ -6,19 +6,6 @@ import PokemonDetails from "./HooksExample/pokedex/page/PokemonDetails";
 
 function App() {
   return (
-    // <div>
-    //   <BrowserRouter>
-    //     <Routes>
-    //       <Route path="/" element={<Layout />}>
-    //         <Route index element={<ShinchanFnadom />} />
-    //         <Route path="character/:name" element={<CharacterDetail />} />
-    //         <Route path="contact" element={<Contact />} />
-    //         <Route path="about" element={<About />} />
-    //       </Route>
-    //     </Routes>
-    //   </BrowserRouter>
-    // </div>
-
     <BrowserRouter>
       <Routes>
         <Route path="/">
